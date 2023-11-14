53c809291cfa587a462ebed7870f9a40dda2d8e4
олег <xfvjykk@yandex.ru>
2023-11-14 22:36:13 +0300

    Pervi wikl

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿string[] array1 = new string[5] {"12345", "darov", "world", "plm", "los"};
-string[] array2 = new string[array1.Length];
+﻿string[] SP1 = new string[5] {"12345", "darov", "world", "plm", "los"};
+string[] SP2 = new string[SP1.Length];
+
+void WIL(string[] SP1, string[] SP2)
+{
+    int count = 0;
+    for (int i = 0; i < SP1.Length; i++)
+    {
+    if(SP1[i].Length <= 3)
+        {
+        SP2[count] = SP1[i];
+        count++;
+        }
+    }
+}
