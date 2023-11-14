9e92cf4547f70c63bba8bfd8d840252e5e34148e
олег <xfvjykk@yandex.ru>
2023-11-14 22:42:35 +0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
-﻿string[] SP1 = new string[5] {"12345", "darov", "world", "plm", "los"};
-string[] SP2 = new string[SP1.Length];
+﻿string[] sp1 = new string[5] {"12345", "darov", "world", "plm", "los"};
+string[] sp2 = new string[sp1.Length];
 
-void WIL(string[] SP1, string[] SP2)
+void Wil(string[] sp1, string[] sp2)
 {
     int count = 0;
-    for (int i = 0; i < SP1.Length; i++)
+    for (int i = 0; i < sp1.Length; i++)
     {
-    if(SP1[i].Length <= 3)
+    if(sp1[i].Length <= 3)
         {
-        SP2[count] = SP1[i];
-        count++;
+        sp2[count] = sp1[i];
+        count =+1;
         }
     }
 }
+void Print(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Wil(sp1, sp2);
+Print(sp2);
